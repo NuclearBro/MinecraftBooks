df58edfdafb7fc99b6a716898de892e3ce5e2d80
Iwann <iwannbadenhorst@gmail.com>
2024-11-29 00:21:43 +0200

    Fixing resize

--- script.js
+++ script.js
@@ -61,4 +61,13 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   showSlides();
+
+function setViewportHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+window.addEventListener('resize', setViewportHeight);
+window.addEventListener('orientationchange', setViewportHeight);
+setViewportHeight(); // Initial call
+
 });
