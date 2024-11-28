146d89a006ce4341be86dd81247fbdbda020a9ec
Iwann <iwannbadenhorst@gmail.com>
2024-11-28 18:08:47 +0200

    Made improvements

--- script.js
+++ script.js
@@ -7,35 +7,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const closeButton = document.getElementById("close-button");
 
   try {
-    const books = await Promise.all([
-      fetch("Books/support101.json").then((res) => res.json()),
-      // fetch("Books/theDrift.json").then((res) => res.json()),
-      // fetch("Books/fate.json").then((res) => res.json()),
-      // fetch("Books/openingUp.json").then((res) => res.json()),
-      // fetch("Books/goodPlace.json").then((res) => res.json()),
-      // fetch("Books/things.json").then((res) => res.json()),
-      // fetch("Books/grandFinale.json").then((res) => res.json()),
-      // fetch("Books/Realization.json").then((res) => res.json()),
-      // fetch("Books/16Dec.json").then((res) => res.json()),
-      // fetch("Books/anonymous.json").then((res) => res.json()),
-      // fetch("Books/attemptHappy.json").then((res) => res.json()),
-      // fetch("Books/badIdea.json").then((res) => res.json()),
-      // fetch("Books/blank.json").then((res) => res.json()),
-      // fetch("Books/fax.json").then((res) => res.json()),
-      // fetch("Books/happyMemories.json").then((res) => res.json()),
-      // fetch("Books/leave.json").then((res) => res.json()),
-      // fetch("Books/memories.json").then((res) => res.json()),
-      // fetch("Books/msgInABottle.json").then((res) => res.json()),
-      // fetch("Books/perfection.json").then((res) => res.json()),
-      // fetch("Books/sadReality.json").then((res) => res.json()),
-      // fetch("Books/thisIsTheEnd.json").then((res) => res.json()),
-      // fetch("Books/thoughts.json").then((res) => res.json()),
-      // fetch("Books/uglyTruth.json").then((res) => res.json()),
-      // fetch("Books/vaca.json").then((res) => res.json()),
-      // fetch("Books/whyNot.json").then((res) => res.json()),
-      // fetch("Books/worstIdeaYet.json").then((res) => res.json()),
-      // fetch("Books/fixSadReality.json").then((res) => res.json()),
-    ]);
+    const books = await fetch("Books/allBooks.json").then((res) => res.json());
 
     books.forEach((book) => {
       const bookItem = document.createElement("div");
@@ -73,21 +45,20 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   let slideIndex = 0;
 
-function showSlides() {
-  let slides = document.getElementsByClassName("mySlides");
-  
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("active");
+  function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1; 
+    }
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 5000);
   }
-  
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1; 
-  }
-  slides[slideIndex - 1].classList.add("active");
-  setTimeout(showSlides, 5000);
-}
 
-showSlides();
-
+  showSlides();
 });
