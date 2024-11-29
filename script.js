2a8abda462c9185b2cf0be368e2a86883775f1e9
Iwann <iwannbadenhorst@gmail.com>
2024-11-29 12:48:19 +0200

    Testing address bar resizing

--- script.js
+++ script.js
@@ -62,4 +62,13 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   showSlides();
 
+  var bg = $("#bg1, #bg2");
+
+  function resizeBackground() {
+    bg.height( $(window).height() + 60);
+  }
+
+  $(window).resize(resizeBackground);
+  resizeBackground();
+
 });
